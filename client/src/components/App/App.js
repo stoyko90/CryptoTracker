18932053221219021f13c848263bf2e8b581ee01
Stoyko <stoyko90@gmail.com>
2021-07-30 15:50:22 +0100

    created react project and formed basic folder structure for the main components

---
+++ client/src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+export default function App() {
+
+
+  return (
+    <div>
+      <p>HIIIIIIIII</p>
+      <h1>WAZAAAAAA</h1>
+      <h2>YOOOOo</h2>
+    </div>
+  )
+}
